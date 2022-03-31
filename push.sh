9b797c6c436ab33ddc371490b902b4cb3f4b9b83
Egor Repiev <egor.repiev@gmail.com>
2022-03-31 17:58:29 +0500

    Изменение push.sh.

--- push.sh
+++ push.sh
@@ -2,3 +2,4 @@
 docker exec nttek_auth rm -rf /var/www/html/*
 docker cp ./html/. nttek_auth:/var/www/html/.
 docker cp ./assets/. nttek_auth:/var/www/html/assets/.
+docker exec nttek_auth chmod -R 777 /var/www/html/
